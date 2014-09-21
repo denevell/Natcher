f3eaa9f34b84ac311931776544ab0588654b1da9
denevell <aaron@denevell.org>
2014-09-21 01:27:24 +0100

    Added more tests for on populate from cache.

---
+++ src/test/java/com/newfivefour/natcher/uicomponent/OnPopulateFromCacheTests.java
@@ -0,0 +1,89 @@
+package com.newfivefour.natcher.uicomponent;
+
+import com.newfivefour.natcher.uicomponent.views.EmptyView;
+import com.newfivefour.natcher.uicomponent.views.LoadingView;
+import com.newfivefour.natcher.uicomponent.views.ServerErrorView;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+@RunWith(RobolectricTestRunner.class)
+@Config(manifest = Config.NONE)
+@SuppressWarnings("unchecked")
+public class OnPopulateFromCacheTests {
+
+    private final Runnable hideKeyboard;
+    private final Populatable populatable;
+    private final UiComponentVanilla<Object> uiComponent;
+    private final LoadingView loaderInComponent;
+    private final LoadingView loaderOutOfComponent;
+    private final EmptyView emptyView;
+    private final ServerErrorView serverErrorViewInComponent;
+    private final ServerErrorView serverErrorViewOutOfComponent;
+
+    public OnPopulateFromCacheTests() {
+        populatable = mock(Populatable.class);
+
+        hideKeyboard = mock(Runnable.class);
+        loaderInComponent = mock(LoadingView.class);
+        loaderOutOfComponent = mock(LoadingView.class);
+        emptyView = mock(EmptyView.class);
+        serverErrorViewInComponent = mock(ServerErrorView.class);
+        serverErrorViewOutOfComponent = mock(ServerErrorView.class);
+
+        uiComponent = new UiComponentVanilla<Object>(populatable);
+        uiComponent.setHideKeyboard(hideKeyboard);
+        uiComponent.setInComponentLoadingDisplay(loaderInComponent);
+        uiComponent.setOutOfComponentLoadingDisplay(loaderOutOfComponent);
+        uiComponent.setOutOfComponentServerErrorDisplay(serverErrorViewOutOfComponent);
+        uiComponent.setInComponentServerErrorDisplay(serverErrorViewInComponent);
+        uiComponent.setEmptyDisplay(emptyView);
+    }
+
+    @Test
+    public void shouldPopulateOnCachedResult() {
+        // Arrange
+        Object object = new Object();
+
+        // Act
+        uiComponent.populateFromCache(object);
+        verify(populatable).populateOnSuccessfulResponse(object);
+    }
+
+    @Test
+    public void shouldHideServerErrors() {
+        // Arrange
+        Object object = new Object();
+
+        // Act
+        uiComponent.populateFromCache(object);
+        verify(serverErrorViewInComponent).showServerError(false);
+        verify(serverErrorViewOutOfComponent).showServerError(false);
+    }
+
+    @Test
+    public void shouldHideEmptyView() {
+        // Arrange
+        Object object = new Object();
+
+        // Act
+        uiComponent.populateFromCache(object);
+        verify(emptyView).showEmpty(false);
+    }
+
+    @Test
+    public void shouldHideInComponentLoader() {
+        // Arrange
+        Object object = new Object();
+
+        // Act
+        uiComponent.populateFromCache(object);
+        verify(loaderInComponent).showLoading(false);
+    }
+
+}
--- src/test/java/com/newfivefour/natcher/uicomponent/OnResetComponentTests.java
+++ src/test/java/com/newfivefour/natcher/uicomponent/OnResetComponentTests.java
@@ -1,8 +1,6 @@
 package com.newfivefour.natcher.uicomponent;
 
-import com.newfivefour.natcher.uicomponent.views.EmptyView;
 import com.newfivefour.natcher.uicomponent.views.LoadingView;
-import com.newfivefour.natcher.uicomponent.views.ServerErrorView;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -11,40 +9,28 @@ import org.robolectric.annotation.Config;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
-import static org.mockito.Mockito.when;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @RunWith(RobolectricTestRunner.class)
 @Config(manifest = Config.NONE)
 @SuppressWarnings("unchecked")
 public class OnResetComponentTests {
 
-    private final Runnable hideKeyboard;
     private final Populatable populatable;
     private final UiComponentVanilla<Object> uiComponent;
     private final LoadingView loaderInComponent;
     private final LoadingView loaderOutOfComponent;
-    private final EmptyView emptyView;
-    private final ServerErrorView serverErrorViewInComponent;
-    private final ServerErrorView serverErrorViewOutOfComponent;
 
     public OnResetComponentTests() {
         populatable = mock(Populatable.class);
 
-        hideKeyboard = mock(Runnable.class);
         loaderInComponent = mock(LoadingView.class);
         loaderOutOfComponent = mock(LoadingView.class);
-        emptyView = mock(EmptyView.class);
-        serverErrorViewInComponent = mock(ServerErrorView.class);
-        serverErrorViewOutOfComponent = mock(ServerErrorView.class);
 
         uiComponent = new UiComponentVanilla<Object>(populatable);
-        uiComponent.setHideKeyboard(hideKeyboard);
         uiComponent.setInComponentLoadingDisplay(loaderInComponent);
         uiComponent.setOutOfComponentLoadingDisplay(loaderOutOfComponent);
-        uiComponent.setOutOfComponentServerErrorDisplay(serverErrorViewOutOfComponent);
-        uiComponent.setInComponentServerErrorDisplay(serverErrorViewInComponent);
-        uiComponent.setEmptyDisplay(emptyView);
     }
 
     @Test
